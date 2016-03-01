b844daf34c815879c10a016e5089eb4f68670bd6
Ben Deverett <bendeverett@gmail.com>
2016-03-01 14:31:57 -0500

    update

--- soup/mpl.py
+++ soup/mpl.py
@@ -17,14 +17,14 @@ def scatter(*args,**kwargs):
 pl.plot = plot
 pl.scatter = scatter
 
-def ticf(both=None, x=None, y=None, ax=None):
+def ticf(both=None, x=None, y=None, ax=None, fig=None):
     # show only every n ticks
 
     if ax is not None:
         axs = [ax]
         fig = ax.figure
     elif ax is None:
-        fig = pl.gcf()
+        fig = fig or pl.gcf()
         axs = fig.get_axes()
 
     if both:
@@ -47,47 +47,49 @@ def ticf(both=None, x=None, y=None, ax=None):
     fig.canvas.draw()
 
 def pretty(fig=None, ax=None, tickin=True, lims=False, xlim=None, ylim=None):
-    if ax:
+    if ax is not None:
+        axs = [ax]
         fig = ax.figure
-    elif fig is None:
-        fig = pl.gcf()
-    if ax is None:
-        ax = pl.gca()
+    elif ax is None:
+        fig = fig or pl.gcf()
+        axs = fig.get_axes()
+
+    for ax in axs:
+
+        # spines
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+
     
-    # ticks
-    if not tickin:
-        for tic in ax.xaxis.get_major_ticks():
-            tic.tick1On = tic.tick2On = False
-        for tic in ax.yaxis.get_major_ticks():
-            tic.tick1On = tic.tick2On = False
-        ax.tick_params(axis='both', which='both', bottom='off', top='off')
+        # ticks
+        if not tickin:
+            for tic in ax.xaxis.get_major_ticks():
+                tic.tick1On = tic.tick2On = False
+            for tic in ax.yaxis.get_major_ticks():
+                tic.tick1On = tic.tick2On = False
+            ax.tick_params(axis='both', which='both', bottom='off', top='off')
 
-    elif tickin:
-        ax.tick_params(axis='both', which='both', direction='in', bottom='on', top='off', left='on', right='off')
+        elif tickin:
+            ax.tick_params(axis='both', which='both', direction='in', bottom='on', top='off', left='on', right='off')
 
-    # borders
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
+        # lims
+        if lims or xlim or ylim:
+            lines=ax.get_lines()
+            x,y = zip(*[[l.get_xdata(),l.get_ydata()] for l in lines])
+            x,y = map(np.concatenate, [x,y])
+            if xlim:
+                ax.set_xlim(xlim)
+            else:
+                rang = np.max(x)-np.min(x)
+                pad = 0.1*rang
+                ax.set_xlim([np.min(x)-pad,np.max(x)+pad])
+            if ylim:
 
-    # lims
-    if lims or xlim or ylim:
-        lines=ax.get_lines()
-        x,y = zip(*[[l.get_xdata(),l.get_ydata()] for l in lines])
-        x,y = map(np.concatenate, [x,y])
-        if xlim:
-            ax.set_xlim(xlim)
-        else:
-            rang = np.max(x)-np.min(x)
-            pad = 0.1*rang
-            ax.set_xlim([np.min(x)-pad,np.max(x)+pad])
-        if ylim:
-
-            ax.set_ylim(ylim)
-        else:
-            rang = np.max(y)-np.min(y)
-            pad = 0.1*rang
-            ax.set_ylim([np.min(y)-pad,np.max(y)+pad])
+                ax.set_ylim(ylim)
+            else:
+                rang = np.max(y)-np.min(y)
+                pad = 0.1*rang
+                ax.set_ylim([np.min(y)-pad,np.max(y)+pad])
 
     pl.margins(0.05)
-
     fig.canvas.draw()
